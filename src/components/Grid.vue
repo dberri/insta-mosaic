c4a7381d7e267c81518d583c34d27b1e0a1084cd
David Berri <dwbwill@gmail.com>
2020-02-07 11:16:31 -0300

    Creates Grid component

---
+++ src/components/Grid.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="Grid">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Grid"
+};
+</script>
+
+<style>
+.Grid {
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  margin: 0 auto;
+  max-width: 960px;
+}
+</style>
