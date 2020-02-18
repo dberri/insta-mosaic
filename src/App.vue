a4db697efff1e9e013a9a003058b5c463424b4d4
David Berri <dwbwill@gmail.com>
2020-02-17 21:38:55 -0300

    Styles the homepage and adds a "logo"

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1>Insta-mosaic</h1>
+    <Form @submit="userName = $event" />
+    <Mosaic :user="userName" />
+  </div>
 </template>
 
 <script>
+import Form from "@/components/Form";
+import Mosaic from "@/components/Mosaic";
 export default {
   name: "app",
-  components: {}
+  components: {
+    Mosaic,
+    Form
+  },
+  data() {
+    return {
+      userName: ""
+    };
+  }
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Lemonada&display=swap");
+
+:root {
+  --primary-color: #2c3e50;
+}
+
+html * {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: var(--primary-color);
+}
+
+#app > h1 {
+  margin-top: 2rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Lemonada", cursive;
 }
 </style>
