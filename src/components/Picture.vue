f285d378939b24bab5342db4dc821c93f0d7b2af
David Berri <dwbwill@gmail.com>
2020-02-17 21:47:30 -0300

    Implements the Picture component

---
+++ src/components/Picture.vue
@@ -0,0 +1,24 @@
+<template>
+  <img class="Picture" :src="url" :alt="accessibilityCaption" />
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      required: true,
+      type: String
+    },
+    accessibilityCaption: {
+      required: true,
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+.Picture {
+  width: 100%;
+}
+</style>
